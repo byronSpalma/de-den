adde12cfa3fa839405aa1b28b11b2b25a45da754
byronSpalma <byron.palma2002@yahoo.se>
2020-11-09 14:15:07 +0100

    shuno

---
+++ de den/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace de_den
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
